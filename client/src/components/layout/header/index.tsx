bf96e273dc154175ab103bc579bd35e947e4d48a
Marat <evolution99@rambler.ru>
2023-05-14 06:50:27 +0300

    Working state of App

--- client/src/components/layout/header/index.tsx
+++ client/src/components/layout/header/index.tsx
@@ -1,41 +1,37 @@
-import React from "react";
-import { useGetIdentity } from "@refinedev/core";
 import { AppBar, Avatar, Stack, Toolbar, Typography } from "@mui/material";
+import { useGetIdentity } from "@refinedev/core";
+import React from "react";
 
-export const Header: React.FC = () => {
-    const { data: user } = useGetIdentity({
-        v3LegacyAuthProviderCompatible: true,
-    });
-    const showUserInfo = user && (user.name || user.avatar);
+export const Header: React.FC<{ isSticky: boolean }> = () => {
+  const { data: user } = useGetIdentity({
+    v3LegacyAuthProviderCompatible: true,
+  });
+  const showUserInfo = user && (user.name || user.avatar);
 
-    return (
-        <AppBar
-            color="default"
-            position="sticky"
-            elevation={0}
-            sx={{ background: "#fcfcf" }}
+  return (
+    <AppBar
+      color="default"
+      position="sticky"
+      elevation={0}
+      sx={{ background: "#fcfcf" }}
+    >
+      <Toolbar>
+        <Stack
+          direction="row"
+          width="100%"
+          justifyContent="flex-end"
+          alignItems="center"
         >
-            <Toolbar>
-                <Stack
-                    direction="row"
-                    width="100%"
-                    justifyContent="flex-end"
-                    alignItems="center"
-                >
-                    {showUserInfo && (
-                        <Stack direction="row" gap="16px" alignItems="center">
-                            {user.avatar && (
-                                <Avatar src={user?.avatar} alt={user?.name} />
-                            )}
-                            {user.name && (
-                                <Typography variant="subtitle2">
-                                    {user?.name}
-                                </Typography>
-                            )}
-                        </Stack>
-                    )}
-                </Stack>
-            </Toolbar>
-        </AppBar>
-    );
+          {showUserInfo && (
+            <Stack direction="row" gap="16px" alignItems="center">
+              {user.avatar && <Avatar src={user?.avatar} alt={user?.name} />}
+              {user.name && (
+                <Typography variant="subtitle2">{user?.name}</Typography>
+              )}
+            </Stack>
+          )}
+        </Stack>
+      </Toolbar>
+    </AppBar>
+  );
 };
